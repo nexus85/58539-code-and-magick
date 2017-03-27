da197ecbd8e278e96126a8a44024f647550e8a28
Anastasia R <sta.sha@mail.ru>
2017-03-27 21:11:15 +0300

    fix codestyle for travis

--- js/stat.js
+++ js/stat.js
@@ -7,9 +7,9 @@ window.renderStatistics = function(ctx, names, times){
   ctx.shadowOffsetY = 10;
   ctx.shadowBlur = 2;
   ctx.shadowColor = 'rgba(0, 0, 0, 0.7)';
-  ctx.strokeStyle = "#000000";
+  ctx.strokeStyle = '#000000';
   ctx.strokeRect(100, 10, 420, 270);
-  ctx.fillStyle = "#FFFFFF";
+  ctx.fillStyle = '#FFFFFF';
   ctx.fillRect(100, 10, 420, 270);
 
   // Write text
@@ -26,40 +26,40 @@ window.renderStatistics = function(ctx, names, times){
   // Get the worst player time
   var maxTime = -1;
 
-  for (var i = 0; i < times.length; i++){
+  for (var i = 0; i < times.length; i++) {
 
-    if (times[i] > maxTime){
+    if (times[i] > maxTime) {
       maxTime = times[i];
     }
 
   }
 
   // Draw players results
-  ctx.fillStyle = "#000000";
-  ctx.textBaseline = "top";
+  ctx.fillStyle = '#000000';
+  ctx.textBaseline = 'top';
   var fontHeight = 20;
   var startX = 140;
   var startY = 100;
   var barWidth = 40;
   var barSpace = 50;
   var histogramHeight = 150;
-  var step = histogramHeight/maxTime;
+  var step = histogramHeight / maxTime;
 
-  for (var i = 0; i < times.length; i++){
+  for (i = 0; i < times.length; i++) {
 
-    if (names[i] === 'Вы'){
+    if (names[i] === 'Вы') {
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
     } else {
       var alpha = 0;
 
-      while(alpha === 0){
+      while (alpha === 0) {
         alpha = Math.random().toFixed(1);
       }
 
       ctx.fillStyle = 'rgba(0, 0, 255, '+ alpha +')';
-    }
+    };
 
-    ctx.fillRect(startX + (barWidth + barSpace) * i,startY + histogramHeight - step * times[i], barWidth, step * times[i]);
+    ctx.fillRect(startX + (barWidth + barSpace) * i, startY + histogramHeight - step * times[i], barWidth, step * times[i]);
     ctx.fillText(times[i].toFixed(0), startX + (barWidth + barSpace) * i, startY + histogramHeight - step * times[i] - fontHeight);
     ctx.fillText(names[i], startX + (barWidth + barSpace) * i, startY + histogramHeight + 5);
   }
